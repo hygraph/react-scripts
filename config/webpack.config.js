ad40d07c615ff60b72125177fdf1e137c2401d8d
Somasundaram Ayyappan <somasundaram321@gmail.com>
2019-03-05 17:43:08 +0530

    use lodash instead of lodash-es

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -274,7 +274,6 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
-        lodash: 'lodash-es',
         '@material-ui/icons': '@material-ui/icons/es',
         '@material-ui/core': '@material-ui/core/es',
         // Webapp specific resolutions
